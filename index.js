a3c9449c33c33112d754e51c70234225ad9a3f9d
lichun(李春) <lichun@500wan.com>
2016-12-05 14:06:40 +0800

    fix bug

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ let https = require('https')
 let fs = require('fs')
 let forward = require('forward-request')
 let express = require('express')
+let fetch = require('node-fetch')
 let app = express()
 
-app.use((req,resp,next)=>{
+app.use((req,response,next)=>{
     if(~req.originalUrl.indexOf('/mapi')){
-        forward.http(req, resp, 'm.500.com', '113.105.144.58', 80, req.originalUrl);
-    }else {
+        forward.http(req, response, 'm.500.com', '113.105.144.58', 80, req.originalUrl);
+    }else if(~req.originalUrl.indexOf('/static')){
+        fetch(`http://wx.500.com${req.originalUrl}`).then((resp)=>resp.text()).then(res=>{
+            response.setHeader('Content-Type','application/json')
+            let datachart = (data)=>{
+                console.log(data)
+                response.end(JSON.stringify(data))
+            };
+            eval(res);
+        })
+    } else{
         next()
     }
 });
@@ -24,7 +34,12 @@ server.listen(443, (err)=>{
         return
     }
     console.log('listening port: 443')
+});
+app.listen(3002, (err)=>{
+    if(err){
+        return
+    }
+    console.log('listening port: 3002')
 })
-
 
 
